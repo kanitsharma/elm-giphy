c8c3c80381edda9a0585a32d39285cb9bfae721a
kanitsharma <kanitshrm09@gmail.com>
2018-08-12 23:27:09 +0530

    Scaled Elm architecture and added routes

--- src/Main.elm
+++ src/Main.elm
@@ -5,34 +5,38 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode as Decode
+import Navigation
 
 
 main : Program Never Model Msg
 main =
-    Html.program
-        { init = init model
+    Navigation.program UrlChange
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
         }
 
 
-init : Model -> ( Model, Cmd Msg )
-init { api_url, searchText, api_key } =
-    ( { model | showLoader = True }, fetchGifs api_url searchText api_key )
-
-
 
 -- MODEL
 
 
-type alias Model =
+type alias HomeModel =
     { searchText : String
     , gifs : List Gif
-    , api_key : String
-    , api_url : String
     , showLoader : Bool
     }
+
+
+type alias AboutModel =
+    { showImgLoader : Bool
+    }
+
+
+type Route
+    = HomeRoute
+    | AboutRoute
 
 
 type alias Gif =
@@ -41,9 +45,35 @@ type alias Gif =
     }
 
 
-model : Model
-model =
-    Model "Dogs" [] "TFY6tJ4s3i9MtFhW897SLn2ydN2Wa2zS" "https://api.giphy.com/v1/gifs/search" False
+type alias Model =
+    { route : Route
+    , api_key : String
+    , api_url : String
+    , homeModel : HomeModel
+    , aboutModel : AboutModel
+    }
+
+
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    let
+        api_url =
+            "https://api.giphy.com/v1/gifs/search"
+
+        api_key =
+            "TFY6tJ4s3i9MtFhW897SLn2ydN2Wa2zS"
+
+        searchText =
+            "Dogs"
+    in
+        ( Model
+            HomeRoute
+            api_key
+            api_url
+            (HomeModel searchText [] True)
+            (AboutModel False)
+        , Cmd.map HomeMsg (fetchGifs api_url searchText api_key)
+        )
 
 
 
@@ -51,6 +81,12 @@ model =
 
 
 type Msg
+    = UrlChange Navigation.Location
+    | HomeMsg HomeMsg
+    | AboutMsg
+
+
+type HomeMsg
     = UpdateText String
     | SearchGif
     | NewGifs (Result Http.Error (List Gif))
@@ -59,47 +95,76 @@ type Msg
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        UpdateText str ->
-            ( { model | searchText = str }, Cmd.none )
+    let
+        homeModel =
+            model.homeModel
 
-        SearchGif ->
-            ( { model | showLoader = True }, fetchGifs model.api_url model.searchText model.api_key )
+        aboutModel =
+            model.aboutModel
+    in
+        case msg of
+            UrlChange location ->
+                ( { model | route = (getRoute location.hash) }, Cmd.none )
 
-        NewGifs (Ok gifs) ->
-            ( { model
-                | gifs = gifs
-                , showLoader = False
-              }
-            , Cmd.none
-            )
+            HomeMsg homeMsg ->
+                (case homeMsg of
+                    UpdateText str ->
+                        ( { model | homeModel = { homeModel | searchText = str } }, Cmd.none )
 
-        NewGifs (Err _) ->
-            ( model, Cmd.none )
+                    SearchGif ->
+                        ( { model | homeModel = { homeModel | showLoader = True } }, Cmd.map HomeMsg (fetchGifs model.api_url homeModel.searchText model.api_key) )
 
-        StopImgLoader id a ->
-            ( { model
-                | gifs =
-                    List.indexedMap
-                        (\i x ->
-                            case i == id of
-                                True ->
-                                    { x | showImgLoader = False }
-
-                                False ->
-                                    x
+                    NewGifs (Ok gifs) ->
+                        ( { model | homeModel = { homeModel | gifs = gifs, showLoader = False } }
+                        , Cmd.none
                         )
-                        model.gifs
-              }
-            , Cmd.none
-            )
+
+                    NewGifs (Err _) ->
+                        ( model, Cmd.none )
+
+                    StopImgLoader id a ->
+                        ( { model
+                            | homeModel =
+                                { homeModel
+                                    | gifs =
+                                        List.indexedMap
+                                            (\i x ->
+                                                case i == id of
+                                                    True ->
+                                                        { x | showImgLoader = False }
+
+                                                    False ->
+                                                        x
+                                            )
+                                            homeModel.gifs
+                                }
+                          }
+                        , Cmd.none
+                        )
+                )
+
+            AboutMsg ->
+                ( model, Cmd.none )
+
+
+getRoute : String -> Route
+getRoute hash =
+    case hash of
+        "#home" ->
+            HomeRoute
+
+        "#about" ->
+            AboutRoute
+
+        _ ->
+            HomeRoute
 
 
 
 --Commands
 
 
-fetchGifs : String -> String -> String -> Cmd Msg
+fetchGifs : String -> String -> String -> Cmd HomeMsg
 fetchGifs api query key =
     let
         url =
@@ -132,15 +197,23 @@ header =
         [ div
             []
             [ text "Elm-giphy" ]
+        , div
+            [ class "right links_container" ]
+            [ viewLink "home", viewLink "about" ]
         ]
 
 
-inputSection : Model -> Html Msg
-inputSection model =
+viewLink : String -> Html Msg
+viewLink name =
+    li [] [ a [ href ("#" ++ name), class "link" ] [ text <| String.toUpper <| name ] ]
+
+
+inputSection : String -> Html HomeMsg
+inputSection searchText =
     Html.form
         [ class "input_container", onWithOptions "submit" { stopPropagation = True, preventDefault = True } (Decode.succeed SearchGif) ]
         [ input
-            [ type_ "text", placeholder "Search ", onInput UpdateText, value model.searchText ]
+            [ type_ "text", placeholder "Search ", onInput UpdateText, value searchText ]
             []
         , input
             [ type_ "submit", Html.Attributes.value "Search", onClick SearchGif ]
@@ -158,7 +231,7 @@ targetSrc =
     Decode.at [ "target", "src" ] Decode.string
 
 
-gifSection : List Gif -> Html Msg
+gifSection : List Gif -> Html HomeMsg
 gifSection gifs =
     div [ class "overflow_container" ]
         [ div [ class "gifs" ]
@@ -192,7 +265,7 @@ gifSection gifs =
         ]
 
 
-loaderSection : Bool -> Html Msg
+loaderSection : Bool -> Html HomeMsg
 loaderSection showLoader =
     case showLoader of
         False ->
@@ -208,11 +281,28 @@ loaderSection showLoader =
                 ]
 
 
-view : Model -> Html.Html Msg
+home : HomeModel -> Html HomeMsg
+home model =
+    div [ class "home_container" ]
+        [ inputSection model.searchText
+        , gifSection model.gifs
+        , loaderSection model.showLoader
+        ]
+
+
+about : AboutModel -> Html Msg
+about model =
+    div [] [ text "About" ]
+
+
+view : Model -> Html Msg
 view model =
     div [ class "main_container" ]
         [ header
-        , inputSection model
-        , gifSection model.gifs
-        , loaderSection model.showLoader
+        , case model.route of
+            HomeRoute ->
+                Html.map HomeMsg (home model.homeModel)
+
+            AboutRoute ->
+                about model.aboutModel
         ]
