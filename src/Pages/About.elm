3e2c57a2f97586229e4030a94696276d8177be65
kanitsharma <kanitshrm09@gmail.com>
2018-08-13 01:29:58 +0530

    updated about page

--- src/Pages/About.elm
+++ src/Pages/About.elm
@@ -1,16 +1,18 @@
 module Pages.About exposing (Model, init, view, update, Msg)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 type alias Model =
     { showImgLoader : Bool
+    , imgSrc : String
     }
 
 
 init : Model
 init =
-    Model True
+    Model True "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Elm_logo.svg/1200px-Elm_logo.svg.png"
 
 
 type Msg
@@ -26,4 +28,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "About" ]
+    div [ class "about_container" ]
+        [ img [ src model.imgSrc, width 300, height 300 ] []
+        , div [ class "about_title" ] [ text "Made with Elm and ♥" ]
+        ]
